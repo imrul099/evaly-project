00205ee2b2bdbcc3f31eee0e53be6b356c2a5333
Imrul099 <imrulmolla99@gmail.com>
2023-09-28 23:43:11 +0600

    added the enter option

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ document.getElementById('btn-submit').addEventListener('click', function(){
 
     const mainContainer = document.getElementById('review');
     mainContainer.appendChild(p);
-    
+
     textBox.value = '';
+});
+
+document.getElementById('text-box').addEventListener('keyup', function(e){
+    if(e.key === 'Enter'){
+        // console.log('Enter key Pressed');
+        const textBox = document.getElementById('text-box');
+    const areaValue = textBox.value;
+    
+    const p = document.createElement('p');
+    p.innerText = areaValue;
+    
+
+    const mainContainer = document.getElementById('review');
+    mainContainer.appendChild(p);
+
+    textBox.value = '';
+
+    }
+
 })
